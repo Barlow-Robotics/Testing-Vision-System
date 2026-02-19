d609f6dce4c6954e17a8a55b2ab5b09c97445d43
BrianWeiss1 <brianweissct@gmail.com>
2026-02-19 14:16:18 -0500

    Added yolom26

--- YOLOCamera.py
+++ YOLOCamera.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo26s.pt")
+model = YOLO("yolo26m.pt")
 
 model.train(data="FRC Scorekeeper 2026.v1-trial1.yolo26/data.yaml", epochs=100, imgsz=640, batch=64, device=0, cache=True, workers=8)
